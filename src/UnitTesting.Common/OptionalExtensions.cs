84f41be96231cafdb003b010842b34946a06b93a
Jezz Santos <jezz.santos@hotmail.com>
2024-10-23 17:38:47 +1300

    Fixed IsNear testing method

--- src/UnitTesting.Common/OptionalExtensions.cs
+++ src/UnitTesting.Common/OptionalExtensions.cs
@@ -10,7 +10,9 @@ public static class OptionalExtensions
     public static AndConstraint<DateTimeAssertions> BeNear(this OptionalAssertions<DateTime> assertions,
         Optional<DateTime> nearbyTime, int precision = 850, string because = "", params object[] becauseArgs)
     {
-        return new DateTimeAssertions(nearbyTime.ValueOrDefault)
+        return new DateTimeAssertions(assertions.Subject.HasValue
+                ? assertions.Subject.Value
+                : null)
             .BeNear(nearbyTime, TimeSpan.FromMilliseconds(precision), because, becauseArgs);
     }
 
