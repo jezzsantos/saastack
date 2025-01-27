e054fd7a0305dd6ca016cc405a1934ff76f20c84
Jezz Santos <jezz.santos@hotmail.com>
2025-01-27 16:03:11 +1300

    Fixed missing Health check endpoint

--- src/ApiHost1/ApiHostModule.cs
+++ src/ApiHost1/ApiHostModule.cs
@@ -5,7 +5,8 @@ using Infrastructure.Web.Hosting.Common;
 namespace ApiHost1;
 
 /// <summary>
-///     Provides a module for common services of a API host
+///     Provides a module for common services of an API host,
+///     such as the <see cref="HealthApi" /> endpoints.
 /// </summary>
 public class ApiHostModule : ISubdomainModule
 {
@@ -13,14 +14,20 @@ public class ApiHostModule : ISubdomainModule
     {
         get
         {
+            // The TestingOnlyApiModule is already used in TESTINGONLY, and it registers the endpoints of this module already.
+            // We do not want to create duplicate endpoints here.
+#if !TESTINGONLY
+            return (app, _) => app.RegisterRoutes();
+#else
             return (_, _) =>
             {
-                // Add you host specific middleware here
+                // Add your host specific middleware here
             };
+#endif
         }
     }
 
-    public Assembly? DomainAssembly => null;
+    public Assembly? DomainAssembly => null; // No domain assembly
 
     public Dictionary<Type, string> EntityPrefixes => new();
 
