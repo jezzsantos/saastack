7ca8d149735c63213d5cc0cd964319712b1a1c3d
Rene Hernández <rhernandez@softherch.com>
2025-02-20 18:55:31 -0600

    feat(domain): add specs for IData, IBlob, and IEvent interfaces

---
+++ src/Infrastructure.Persistence.Shared.IntegrationTests/OnPremises/SqlServerBlobStoreSpec.cs
@@ -0,0 +1,55 @@
+using FluentAssertions;
+using Infrastructure.Persistence.OnPremises;
+using JetBrains.Annotations;
+using UnitTesting.Common.Validation;
+using Xunit;
+
+namespace Infrastructure.Persistence.Shared.IntegrationTests.OnPremises;
+
+[Trait("Category", "Integration.Persistence")]
+[Collection("SqlServerStore")]
+[UsedImplicitly]
+public class SqlServerBlobStoreSpec : AnyBlobStoreBaseSpec
+{
+    private readonly SqlServerStorageSpecSetup _setup;
+
+    public SqlServerBlobStoreSpec(SqlServerStorageSpecSetup setup) : base(setup.BlobStore)
+    {
+        _setup = setup;
+    }
+
+    [Fact]
+    public async Task WhenDownloadWithInvalidContainerName_ThenThrows()
+    {
+        await _setup.BlobStore
+            .Invoking(
+                x => x.DownloadAsync("^aninvalidcontainername^", "ablobname", Stream.Null, CancellationToken.None))
+            .Should().ThrowAsync<ArgumentOutOfRangeException>()
+            .WithMessageLike(Resources.ValidationExtensions_InvalidStorageAccountResourceName);
+    }
+
+    [Fact]
+    public async Task WhenUploadWithInvalidContainerName_ThenThrows()
+    {
+        await _setup.BlobStore
+            .Invoking(x =>
+            {
+                using var stream = new MemoryStream();
+                return x.UploadAsync("^aninvalidcontainername^", "ablobname", "aconttenttype", stream,
+                    CancellationToken.None);
+            })
+            .Should().ThrowAsync<ArgumentOutOfRangeException>()
+            .WithMessageLike(Resources.ValidationExtensions_InvalidStorageAccountResourceName);
+    }
+
+    [Fact]
+    public async Task WhenDestroyAllWithInvalidContainerName_ThenThrows()
+    {
+#if TESTINGONLY
+        await _setup.BlobStore
+            .Invoking(x => x.DestroyAllAsync("^aninvalidcontainername^", CancellationToken.None))
+            .Should().ThrowAsync<ArgumentOutOfRangeException>()
+            .WithMessageLike(Resources.ValidationExtensions_InvalidStorageAccountResourceName);
+#endif
+    }
+}
