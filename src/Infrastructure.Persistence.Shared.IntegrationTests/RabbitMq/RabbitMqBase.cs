613ae625ed31291b920a56bc9727c4b8cc1bab64
Rene Hernández <rhernandez@softherch.com>
2025-02-22 22:43:14 -0600

    test(rabbitmq): Add integration tests for RabbitMQ.

---
+++ src/Infrastructure.Persistence.Shared.IntegrationTests/RabbitMq/RabbitMqBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Persistence.Shared.IntegrationTests.RabbitMq;
+
+public static class RabbitMqBase
+{
+    public static void CleanupAllTests()
+    {
+    }
+
+    public static void InitializeAllTests()
+    {
+    }
+}
---
+++ src/Infrastructure.Persistence.Shared.IntegrationTests/RabbitMq/RabbitMqMessageBusStoreSpec.cs
@@ -0,0 +1,87 @@
+﻿using Common;
+using FluentAssertions;
+using Infrastructure.Persistence.RabbitMq;
+using UnitTesting.Common.Validation;
+using Xunit;
+
+namespace Infrastructure.Persistence.Shared.IntegrationTests.RabbitMq;
+
+[Trait("Category", "Integration.Persistence")]
+[Collection("RabbitMqMessageBus")]
+public class RabbitMqMessageBusStoreSpec : AnyMessageBusStoreBaseSpec
+{
+    private readonly RabbitMqMessageBusSpecSetup _setup;
+
+    public RabbitMqMessageBusStoreSpec(RabbitMqMessageBusSpecSetup setup) : base(setup.MessageBusStore)
+    {
+        _setup = setup;
+    }
+
+    [Fact]
+    public async Task WhenSendWithInvalidTopicName_ThenThrows()
+    {
+        await _setup.MessageBusStore
+            .Invoking(x => x.SendAsync("^aninvalidtopicname^", "amessage", CancellationToken.None))
+            .Should().ThrowAsync<ArgumentOutOfRangeException>()
+            .WithMessageLike(Resources.ValidationExtensions_InvalidMessageBusTopicName);
+    }
+
+    [Fact]
+    public async Task WhenReceiveSingleWithInvalidTopicName_ThenThrows()
+    {
+#if TESTINGONLY
+        await _setup.MessageBusStore
+            .Invoking(x =>
+                x.ReceiveSingleAsync("^aninvalidtopicname^", "asubscriptionname", (_, _) => Task.FromResult(Result.Ok),
+                    CancellationToken.None))
+            .Should().ThrowAsync<ArgumentOutOfRangeException>()
+            .WithMessageLike(Resources.ValidationExtensions_InvalidMessageBusTopicName);
+#endif
+    }
+
+    [Fact]
+    public async Task WhenReceiveSingleWithInvalidSubscriptionName_ThenThrows()
+    {
+#if TESTINGONLY
+        await _setup.MessageBusStore
+            .Invoking(x =>
+                x.ReceiveSingleAsync("atopicname", "^asubscriptionname^", (_, _) => Task.FromResult(Result.Ok),
+                    CancellationToken.None))
+            .Should().ThrowAsync<ArgumentOutOfRangeException>()
+            .WithMessageLike(Resources.ValidationExtensions_InvalidMessageBusSubscriptionName);
+#endif
+    }
+
+    [Fact]
+    public async Task WhenCountWithInvalidTopicName_ThenThrows()
+    {
+#if TESTINGONLY
+        await _setup.MessageBusStore
+            .Invoking(x => x.CountAsync("^aninvalidtopicname^", "asubscriptionname", CancellationToken.None))
+            .Should().ThrowAsync<ArgumentOutOfRangeException>()
+            .WithMessageLike(Resources.ValidationExtensions_InvalidMessageBusTopicName);
+#endif
+    }
+
+    [Fact]
+    public async Task WhenCountWithInvalidSubscriptionName_ThenThrows()
+    {
+#if TESTINGONLY
+        await _setup.MessageBusStore
+            .Invoking(x => x.CountAsync("atopicname", "^aninvalidsubscriptionname^", CancellationToken.None))
+            .Should().ThrowAsync<ArgumentOutOfRangeException>()
+            .WithMessageLike(Resources.ValidationExtensions_InvalidMessageBusSubscriptionName);
+#endif
+    }
+
+    [Fact]
+    public async Task WhenDestroyAllWithInvalidTopicName_ThenThrows()
+    {
+#if TESTINGONLY
+        await _setup.MessageBusStore
+            .Invoking(x => x.DestroyAllAsync("^aninvalidtopicname^", CancellationToken.None))
+            .Should().ThrowAsync<ArgumentOutOfRangeException>()
+            .WithMessageLike(Resources.ValidationExtensions_InvalidMessageBusTopicName);
+#endif
+    }
+}
