64a9be619aa037fb3b434aef2becc69e17e06012
Rene Hernández <rhernandez@softherch.com>
2025-03-14 14:52:50 -0600

    Add RabbitMQ integration tests for OnPremises (`RabbitMqBase.cs`, `RabbitMqMessageBusStoreSetup.cs`, `RabbitMqMessageBusStoreSpec.cs`, `RabbitMqQueueSetupSpec.cs`, `RabbitMqQueueStoreSpec.cs`)

---
+++ src/Infrastructure.Persistence.Shared.IntegrationTests/OnPremises/RabbitMqBase.cs
@@ -0,0 +1,12 @@
+namespace Infrastructure.Persistence.Shared.IntegrationTests.OnPremises;
+
+public static class RabbitMqBase
+{
+    public static void CleanupAllTests()
+    {
+    }
+
+    public static void InitializeAllTests()
+    {
+    }
+}
